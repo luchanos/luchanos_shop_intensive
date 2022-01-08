6ad87992340482b6029eccf4535335d22c8bedd6
luchanos <Forgotten1000>
2022-01-08 21:12:04 +0300

    [upd] cw 4

---
+++ lesson_4/cw.py
@@ -0,0 +1,18 @@
+import json
+
+my_dict = {
+    "first_key": 1,
+    "second_key": 2,
+    "third_key": {
+        "inner_key_one": 11,
+        "inner_key_two": 22
+    }
+}
+
+with open("my_data.json", "w") as json_file:
+    json.dump(my_dict, json_file, indent=4, ensure_ascii=False)
+
+with open("my_data_2.json", "r") as json_file:
+    my_data_from_json = json.load(json_file)
+
+print(my_data_from_json)
